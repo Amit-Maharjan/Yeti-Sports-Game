932dc14a825a8c847f186e8984a9f1e22770ed7b
Amit Maharjan <maharjanamit8@gmail.com>
2019-05-11 13:57:01 +0545

    Game Over Box Added

--- script.js
+++ script.js
@@ -769,12 +769,59 @@ function displayLives() {
   ctx.fillText('Lives : ' + lives, 10, 50);
 }
 
+let xPositionOfGameOver = canvas.width + crane.width;
+let xDecrementOfGameOver = 0;
+
+function displayGameOver() {
+  ctx.beginPath();
+  ctx.moveTo(
+    crane.x + crane.width - 50,
+    yPositionOfPenguin + penguin.imageHeight - 60
+  );
+  ctx.lineTo(
+    xPositionOfGameOver - xDecrementOfGameOver + 100,
+    yPositionOfPenguin + penguin.imageHeight - 60
+  );
+  ctx.strokeStyle = 'black';
+  ctx.stroke();
+  ctx.closePath();
+
+  ctx.fillStyle = 'grey';
+  ctx.fillRect(
+    xPositionOfGameOver - xDecrementOfGameOver + 100,
+    yPositionOfPenguin + penguin.imageHeight - 120,
+    155,
+    100
+  );
+
+  ctx.font = '30px arial';
+  ctx.fillStyle = 'Red';
+  ctx.fillText(
+    'Game Over',
+    xPositionOfGameOver - xDecrementOfGameOver + 100,
+    yPositionOfPenguin + penguin.imageHeight - 90
+  );
+  ctx.fillText(
+    'Score : ' + Math.ceil(score / 100),
+    xPositionOfGameOver - xDecrementOfGameOver + 100,
+    yPositionOfPenguin + penguin.imageHeight - 60
+  );
+  ctx.fillText(
+    'Play Again',
+    xPositionOfGameOver - xDecrementOfGameOver + 100,
+    yPositionOfPenguin + penguin.imageHeight - 30
+  );
+}
+
 function gameOver() {
   if (crane.x >= 300) {
     crane.x -= 5;
+    xDecrementOfGameOver += 5;
     crane.updateCraneSprite();
   }
+
   crane.drawCrane();
+  displayGameOver();
 }
 
 let animationNumber = 1;
